1befa0380dc4623bd72734dc587f2e6914c1cf13
Aurelien Senn <aurelien.senn.web@gmail.com>
2022-10-03 10:24:23 +0200

    add login with jwt

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,37 +1,79 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
+import axios from '../../api/axios';
 import { UserContext } from '../../context/userContext'
 
+const REGISTER_URL = '/api/auth/login';
 
 export default function Login() {
+    const [success, setSuccess] = useState(false);
     const { toggleModals, modalState } = useContext(UserContext)
+    const [validation, setValidation] = useState('');
+    const inputs = useRef([])
 
+    const addInputs = el => {
+        if (el && !inputs.current.includes(el)) {
+            inputs.current.push(el)
+        }
+    }
+    const handleForm = async (e) => {
+        e.preventDefault()
+        try {
+            const email = inputs.current[0].value;
+            const password = inputs.current[1].value;
+            const data = JSON.stringify({ email: email, password: password });
+            console.log(data);
+            await axios.post(REGISTER_URL, { data })
+                .then(res => {
+                    localStorage.setItem("user", JSON.stringify(res.data));
+                })
+
+            console.log('ok');
+            setSuccess(true);
+
+        } catch (err) {
+            if (!err?.response) {
+                setValidation('pas de reponse serveur');
+            } else if (err.response?.status === 409) {
+                setValidation('Couple mot de passe/email incorrect');
+            } else {
+                setValidation('Echec de la connexion')
+            }
+        }
+    }
     return (
         <>
-            {modalState.signInModal && (
-                <div>
-                    <h5>Connexion</h5>
-                    <button onClick={() => toggleModals("close")}>fermer</button>
-                    <form action="">
-                        <label htmlFor="signInEmail">adresse mail</label>
-                        <input
-                            type="email"
-                            name="email"
-                            required
-                            id="signInEmail"
-                        />
-                        <label htmlFor="signUpPwd">Mot de passe</label>
-                        <input
-                            type="password"
-                            name="pwd"
-                            required
-                            id="signUpPwd"
-                        />
+            {success ? (
+                <h1>Connexion réussit</h1>
+            ) : (
+                modalState.signInModal && (
+                    <div>
+                        <h5>Connexion</h5>
+                        <button onClick={() => toggleModals("close")}>fermer</button>
+                        <form onSubmit={handleForm}>
+                            <label htmlFor="signInEmail"> Email :</label>
+                            <input
+                                ref={addInputs}
+                                type="email"
+                                name="email"
+                                required
+                                id="signInEmail"
+                            />
+                            <label htmlFor="signUpPwd"> Mot de passe :</label>
+                            <input
+                                ref={addInputs}
+                                type="password"
+                                name="pwd"
+                                required
+                                id="signInPwd"
+                            />
 
-                        <p>Validé</p>
-                        <button>Soumettre</button>
-                    </form>
-                </div>
-            )}
+                            <p>{validation}</p>
+                            <button>Soumettre</button>
+                        </form>
+                    </div>
+                )
+            )
+            }
         </>
     );
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,6 +1,14 @@
 import React, { useContext, useRef, useState } from 'react';
+import axios from '../../api/axios';
 import { UserContext } from '../../context/userContext'
-export default function Login() {
+
+const USER_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/api/auth/signup';
+
+
+export default function Register() {
+    const [success, setSuccess] = useState(false);
     const { toggleModals, modalState } = useContext(UserContext)
     const [validation, setValidation] = useState('');
     const inputs = useRef([])
@@ -11,53 +19,94 @@ export default function Login() {
     }
     const handleForm = async (e) => {
         e.preventDefault()
-
+        const v1 = USER_REGEX.test(inputs.current[0].value);
+        const v2 = PWD_REGEX.test(inputs.current[1].value);
+        console.log(inputs.current[1].value);
         if ((inputs.current[1].value.length || inputs.current[2].value.length) < 8) {
             setValidation("8 caractères minimum")
+
             return;
         }
         else if (inputs.current[1].value.length !== inputs.current[2].value.length) {
             setValidation("les mots de passes ne correspondent pas")
+
             return;
+        } else if (!v1 || !v2) {
+            setValidation("le mot de passe doit contenir minimum: une minuscule, une majuscule, un chiffre et un caractère spécial");
+            inputs = [];
+            return;
+        } try {
+            const email = inputs.current[0].value;
+            const password = inputs.current[1].value;
+            const data = JSON.stringify({ email: email, password: password });
+            console.log(data);
+            await axios.post(REGISTER_URL, { data })
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                })
+
+            console.log('ok');
+            setSuccess(true);
+
+        } catch (err) {
+            if (!err?.response) {
+                setValidation('pas de reponse serveur');
+            } else if (err.response?.status === 409) {
+                setValidation('Email déja utilisé');
+            } else {
+                setValidation('Echec de l\'inscription')
+            }
         }
     }
+
+
+
     return (
         <>
-            {modalState.signUpModal && (
-                <div>
-                    <h5>Inscription</h5>
-                    <button onClick={() => toggleModals("close")}>fermer</button>
-                    <form onSubmit={handleForm}>
-                        <label htmlFor="signUpEmail">adresse mail</label>
-                        <input
-                            ref={addInputs}
-                            type="email"
-                            name="email"
-                            required
-                            id="signUpEmail"
-                        />
-                        <label htmlFor="signUpPwd">Mot de passe</label>
-                        <input
-                            ref={addInputs}
-                            type="password"
-                            name="pwd"
-                            required
-                            id="signUpPwd"
-                        />
-                        <label htmlFor="repeatPwd">répétez le Mot de passe</label>
-                        <input
-                            ref={addInputs}
-                            type="password"
-                            name="pwd"
-                            required
-                            id="repeatPwd"
-                        />
-                        <p>{validation}</p>
-                        <button>Soumettre</button>
-                    </form>
-                </div>
-            )}
+            {success ? (
+                <h1>Inscription réussit</h1>
+            ) : (
+                modalState.signUpModal && (
+                    <div>
+                        <h5>Inscription</h5>
+                        <button onClick={() => toggleModals("close")}>fermer</button>
+                        <form onSubmit={handleForm}>
+                            <label htmlFor="signUpEmail"> Email : </label>
+                            <input
+                                ref={addInputs}
+                                type="email"
+                                name="email"
+                                required
+                                id="signUpEmail"
+                            />
+                            <label htmlFor="signUpPwd"> Mot de passe : </label>
+                            <input
+                                ref={addInputs}
+                                type="password"
+                                name="pwd"
+                                required
+                                id="signUpPwd"
+                            />
+                            <label htmlFor="repeatPwd">répétez le Mot de passe</label>
+                            <input
+                                ref={addInputs}
+                                type="password"
+                                name="pwd"
+                                required
+                                id="repeatPwd"
+                            />
+                            <p>{validation}</p>
+                            <button>Soumettre</button>
+                        </form>
+                    </div>
+                )
+            )
+            }
         </>
+
     );
+
 }
+
 
